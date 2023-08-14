167647ce9c417d36deb24db2ad1aa72f460bf379
Akhila Indula Gunawardena <79861435+akhilaguna@users.noreply.github.com>
2023-08-14 15:01:44 +0530

    Added all project files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let count = 0;
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment(){
+    count += 1
+    countEl.textContent = count
+}
+
+function save(){
+    let lastCount = " "+count + " - "
+    saveEl.textContent += lastCount
+    count = 0
+    countEl.textContent = count
+}
+
+
+
+
